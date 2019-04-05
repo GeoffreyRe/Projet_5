075752ee7d222ac87fcc1931eeb26905f1c1d5d4
Geoffrey Remacle <geoffreyremacle@outlook.fr>
2019-04-05 16:13:28 +0200

    finalisation of class API and class Check

--- API.py
+++ API.py
@@ -1,20 +1,17 @@
+
 import requests
+import Check 
 class API(object):
 	def __init__(self):
 		self.products_list =[]
-		self.variables_list = ["_id", "nutrition_grades", "product_name", "url", "stores", "nutriments"]
-		self.variables_list_2 = [0,0,0,0,0,"nutrition-score-fr"]
-		self.stores_list = ["Carrefour", "Carrefour Market", "Carrefour Planet",
-		 "Carrefour Express", "Carrefour GB", "Colruyt", "Spa",
-		 "Delhaize", "Delhaize City","Proxy Delhaize", "AD Delhaize", "Shop'n Go",
-		 "Albert Heijn", "Intermarché", "Cora", "Match", "Smatch" , "Louis Delhaize",
-		 "Aldi", "Lidl", "Magasins U", "Super U", "Hyper U", "Auchan", "Noz", "Casino",
-		 "Leclerc", "Géant", "Dia", "Edeka", "Magasins diététiques"]
+		self.variables_list = ["_id", "nutrition_grades", "product_name", "url", "stores", "brands", "nutriments"]
+		self.variables_list_2 = [0,0,0,0,0,0,"nutrition-score-fr"]
+		self.checker = Check.Check()
 
 	def get_request_product(self,categorie ,nutriscore):
 		http_link = ("https://be-fr.openfoodfacts.org/cgi/search.pl?search_simple=1&action=process&"
 		"tagtype_0=categories&tag_contains_0=contains&tag_0={}&tagtype_1=nutrition_grades&tag_contains_1=contains"
-		"&tag_1={}&sort_by=unique_scans_n&page_size=150&json=1")
+		"&tag_1={}&sort_by=unique_scans_n&page_size=200&json=1")
 		try:
 			request = requests.get(http_link.format(categorie,nutriscore))
 		except:
@@ -38,59 +35,29 @@ class API(object):
 
 		return str(dictionary_value)
 
-	def separate_stores(self,stores):
-		stores_list_formatted = []
-		stores_list = stores.split(",")
-		for store in stores_list:
-			stores_list_formatted.append(store.strip())
-		return stores_list_formatted
-
-	def title(self, str_list):
-		title_list = []
-		for string in str_list:
-			title_list.append(string.title())
-		return title_list
-
-	def verify_stores(self, stores):
-		verified_stores = []
-		stores = self.separate_stores(stores)
-		for store in stores:
-			if store == "":
-				return ""
-			elif store in (self.stores_list + self.title(self.stores_list)):
-				verified_stores.append(store)
-		if len(verified_stores) >= 1: 
-			return verified_stores
-		else:
-			return ""
-
-
-
-	def find_informations(self):
+	def find_informations(self,categorie, nutriscore):
+		self.get_request_product(categorie,nutriscore) 
 		for product in self.products:
 			variables_of_a_product = []
 			for i, variable_name in enumerate(self.variables_list):
 				value = self.find_value(product, variable_name, self.variables_list_2[i]).strip()
-				if variable_name == "stores":
-					value = self.verify_stores(value)
 				variables_of_a_product.append(value)
+			self.products_list.append(variables_of_a_product)
+		self.use_the_checker()
 
-			if "" in variables_of_a_product:
-				pass
-			else:
-				self.products_list.append(variables_of_a_product)
+	def use_the_checker(self):
+		self.products_list = self.checker.complete_check(self.products_list)
 
-openfoodfact = API()
-openfoodfact.get_request_product("crèmes", "d")
-openfoodfact.find_informations()
-for produit in openfoodfact.products_list:
-	print(produit)
+if __name__ == "__main__": 
+	openfoodfact = API()
+	openfoodfact.find_informations("Aliments d'origine végétale", "a")
+	a = 0
+	for produit in openfoodfact.products_list:
+		if "" in produit:
+			a +=1
 
-
-
-
-
-
+	print(a)
+	print(len(openfoodfact.products_list))
 
 
 
--- Check.py
+++ Check.py
@@ -1,12 +1,14 @@
 class Check(object):
 	""" Class that will verify values from Openfoodfacts API"""
 	def __init__(self):
-		self.stores_list = ["Carrefour", "Carrefour Market", "Carrefour Planet",
-		 "Carrefour Express", "Carrefour GB", "Colruyt", "Spa",
-		 "Delhaize", "Delhaize City","Proxy Delhaize", "AD Delhaize", "Shop'n Go",
-		 "Albert Heijn", "Intermarché", "Cora", "Match", "Smatch" , "Louis Delhaize",
-		 "Aldi", "Lidl", "Magasins U", "Super U", "Hyper U", "Auchan", "Noz", "Casino",
-		 "Leclerc", "Géant", "Dia", "Edeka", "Magasins diététiques"]
+		#self.api= openfoodfact.API()
+		self.variables_list = self.variables_list = ["_id", "nutrition_grades", "product_name", "url", "stores", "brands", "nutriments"]
+		self.stores_list = ["carrefour", "carrefour market", "carrefour planet",
+		 "carrefour express", "carrefour GB", "colruyt", "spa",
+		 "delhaize", "delhaize city","proxy delhaize", "aD delhaize", "shop'n go",
+		 "albert heijn", "intermarché", "cora", "match", "smatch" , "louis delhaize",
+		 "aldi", "lidl", "magasins U", "super U", "hyper U", "auchan", "noz", "casino",
+		 "leclerc", "géant", "dia", "edeka", "magasins diététiques"]
 
 	def capital_letter(self, str_list):
 		""" method that put capital letter at 
@@ -31,13 +33,47 @@ class Check(object):
 		for store in stores:
 			if store == "":
 				return ""
-			elif store in (self.stores_list or self.title(self.stores_list)):
+			elif store in (self.stores_list + self.capital_letter(self.stores_list)):
 				verified_stores.append(store)
 		if len(verified_stores) >= 1: 
 			return verified_stores
 		else:
 			return ""
 
-	def remove_bad_products(self):
+	def remove_bad_products(self,list_products):
+		good_products = []
 		"""method that remove products that don't satisfy conditions"""
+		for i,product in enumerate(list_products):
+			if "" in product:
+				pass
+			else:
+				good_products.append(product)
+		return good_products
+
+	def separate_brands(self, brands):
+		if "," in brands:
+			brands_list = brands.split(",")
+			return brands_list[0].strip()
+		return brands
+			
+
+	def complete_check(self,list_products):
+		verified_products = []
+		for product in list_products:
+			verified_product = []
+			for i, variable in enumerate(product):
+				if i == self.variables_list.index("stores"):
+					variable = self.verify_stores(variable)
+				elif i == self.variables_list.index("brands"):
+					variable = self.separate_brands(variable)
+				verified_product.append(variable)
+			verified_products.append(verified_product)
+		verified_products = self.remove_bad_products(verified_products)
+		return verified_products
+
+
+
+
+
+
 		
